9c4e2c1540884ca0a0d49f93bb139fceab0320f0
Anurag Pal <palanurag595@gmail.com>
2025-06-20 15:09:00 +0530

    Setup the Game Window

--- dodge the box_1.py
+++ dodge the box_1.py
@@ -1,4 +1,15 @@
 import pygame
 import random
 import sys
+pygame.init()
 
+Width, Height = 500, 600
+win = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Dodge the box")
+
+clock = pygame.time.clock()
+
+blue = (0, 0, 255)
+red = (255, 0, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
